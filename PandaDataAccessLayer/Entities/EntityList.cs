a30230943aba86313ef5a8923096d07f4d4995b2
redrick-tmn <redrick.tmn@gmail.com>
2013-08-30 00:20:56 +0600

    Attributes added.

--- PandaDataAccessLayer/Entities/EntityList.cs
+++ PandaDataAccessLayer/Entities/EntityList.cs
@@ -14,11 +14,17 @@ namespace PandaDataAccessLayer.Entities
         public Guid Id { get; set; }
 
         public virtual ICollection<WorkExpirience> WorkExpirience { get; set; }
+        public virtual ICollection<DesiredWork> DesiredWork { get; set; }
+        public virtual ICollection<DesiredWorkTime> DesiredWorkTime { get; set; }
 
         public EntityList() 
         {
             if (WorkExpirience == null)
                 WorkExpirience = new List<WorkExpirience>();
+            if (DesiredWork == null)
+                DesiredWork = new List<DesiredWork>();
+            if (DesiredWorkTime == null)
+                DesiredWorkTime = new List<DesiredWorkTime>();
         }
     }
 }
